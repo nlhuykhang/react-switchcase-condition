4f070ce95df2fe69197560838f2bbf6b5498e2a2
Khang Nguyen-Le <nlhuykhang@gmail.com>
2017-07-10 13:36:24 +0700

    Remove unused test case

--- __tests__/boilerplate_test.js
+++ __tests__/boilerplate_test.js
@@ -6,12 +6,10 @@
 //   Simulate
 // } from 'react-addons-test-utils';
 import { expect } from 'chai';
-import { demo } from '../src';
 
 describe('Boilerplate', function () {
   it('should do boilerplate things', function () {
     // TODO: test something now
     expect(true).to.equal(true);
-    expect(demo()).to.equal('what the hell? how do I test my component???');
   });
 });
