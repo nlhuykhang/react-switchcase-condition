ded8be3aa9145b9b15105aa84cfb561efe25ba92
Khang Nguyen-Le <nlhuykhang@gmail.com>
2017-07-10 12:36:48 +0700

    Add Case component

--- src/case.js
+++ src/case.js
@@ -1,27 +1,31 @@
-import React, { Component } from 'react';
+import React, {
+  Component,
+  // cloneElement,
+} from 'react';
+import PropTypes from 'prop-types';
 
 export default class Case extends Component {
-  render() {}
+  componentDidMount() {}
+  render() {
+    const {
+      component: C,
+      componentProps,
+    } = this.props;
+
+    return (
+      <div className="">
+        <C {...componentProps} />
+      </div>
+    );
+  }
 }
 
-/*
+Case.propTypes = {
+  component: PropTypes.func.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  componentProps: PropTypes.object,
+};
 
-ok what is the problem here?
-
-Should comopnent be rendered by Case or Switch?
-
-I think it should be Case right?
-
-If so what does Switch do??
-
-I am think about:
-
-<Switch>
-  <Case cond={} component={} />
-</Switch>
-
-
-hmmm, maybe Switch will need to look into its children to determine
-which one get rendered, also to delay render
-
-*/
+Case.defaultProps = {
+  componentProps: {},
+};
