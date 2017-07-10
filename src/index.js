a5206ed3d3c3af4cbbe6347d3bae06213613052f
Khang Nguyen-Le <nlhuykhang@gmail.com>
2017-07-10 12:40:20 +0700

    Update Switch component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-// this should be the entry point to your library
 module.exports = {
-  demo: require('./demo').default,
   Switch: require('./switch').default,
-  Case: require('./case').default
+  Case: require('./case').default,
+  Default: require('./default').default,
 };
--- src/switch.js
+++ src/switch.js
@@ -1,11 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, Children } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Switch extends Component {
+  componentDidMount() {}
   render() {
+    const children = Children.toArray(this.props.children);
+    const caseToRender = children.filter(
+      child => child.type.displayName === 'Case' && child.props.cond
+    )[0];
+
+    const defaultChild = children.filter(child => child.type.displayName === 'Default');
+
+    if (
+      defaultChild.length !== 1 ||
+      !defaultChild[0]
+    ) {
+      throw new Error('There has to be exact one Default component');
+    }
+
+    const childToRender = caseToRender || defaultChild;
+
     return (
       <div className="">
-        this is the switch component
+        {childToRender}
       </div>
     );
   }
 }
+
+Switch.propTypes = {
+  children: PropTypes.arrayOf(function (props, propName, componentName) {
+    let error;
+    const prop = props[propName];
+
+    React.Children.forEach(prop, function (child) {
+      if (
+        !child ||
+        !child.type ||
+        !(child.type.displayName === 'Case' ||
+        child.type.displayName === 'Default')
+      ) {
+        error = new Error(`${componentName} only accepts children of type Case or Default.`);
+      }
+    });
+
+    return error;
+  }).isRequired,
+};
